4db5ba1793ea08513bf7b1151cf377de7c37f641
Jsagwa8781 <105121061+Jsagwa8781@users.noreply.github.com>
2024-03-19 21:42:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import calendar
 from datetime import datetime
 import streamlit as st
-from streamlit_option_menu import option_menu
 import plotly
     #---Creating the structure of the webpage---
 incomes = ["Salary", "Side Hustle", "Other Income"]
